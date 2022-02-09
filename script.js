6fd76bded9b8f89cd5312e1787e646d03a3d9326
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-09 15:34:32 +0600

    Added example8 to index.html

--- script.js
+++ script.js
@@ -25,34 +25,55 @@
 // })
 
 
-let num = 0;
+// let num = 0;
 
-let h1 = document.querySelector('h1');
-let resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', function() {
-  h1.textContent = num;
-});
+// let h1 = document.querySelector('h1');
+// let resetButton = document.querySelector('#reset');
+// resetButton.addEventListener('click', function() {
+//   h1.textContent = num;
+// });
 
-let increaseButton = document.querySelector('#increase');
+// let increaseButton = document.querySelector('#increase');
 
-increaseButton.addEventListener('click', function(){
-  num = num + 1;
-  h1.textContent = num;
-});
+// increaseButton.addEventListener('click', function(){
+//   num = num + 1;
+//   h1.textContent = num;
+// });
 
-// num += 1
-// num++
-let decreaseButton = document.querySelector('#decrease');
+// // num += 1
+// // num++
+// let decreaseButton = document.querySelector('#decrease');
 
-decreaseButton.addEventListener('click', function(){
-  num = num - 1;
-  h1.textContent = num;
-});
-
-
-resetButton.addEventListener('click', function(){
-  num = 0;
-  h1.textContent = num;
-});
+// decreaseButton.addEventListener('click', function(){
+//   num = num - 1;
+//   h1.textContent = num;
+// });
 
 
+// resetButton.addEventListener('click', function(){
+//   num = 0;
+//   h1.textContent = num;
+// });
+
+
+
+
+// let example6Button = document.querySelector('#examle6');
+// example6Button.addEventListener('click', function() {
+//   example6Button.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+// });
+
+
+// let example7Button = document.querySelector('#examle7');
+// example7Button.addEventListener('click', function() {
+//   example7Button.remove();
+// });
+
+
+let example8Button = document.querySelector('#example8');
+function example8Action() {
+
+  alert("hello");
+  example8Button.removeEventListener('click', example8Action);
+}
+ example8Button.addEventListener('click', example8Action);
