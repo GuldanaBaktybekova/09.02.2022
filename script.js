5ae22a7107d13d45f830c7afa3a8c3cc66462bb5
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-09 14:18:45 +0600

    Added ex3Button to script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let example2Button = document.getElementById('example2');
 example2Button.onclick = function() {
   alert("Nice to meet you!");
 }
+
+let example3Button = document.getElementById('example3');
+example3Button.addEventListener('click' , function() {
+  alert("Bye!");
+
+});
